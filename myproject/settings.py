66a027a37cb811c443b86c295fd65ea0899b1284
sjukic <sadik.jukic@teladapters.com>
2024-07-15 22:34:06 +0200

    added allow cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'books',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +80,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
